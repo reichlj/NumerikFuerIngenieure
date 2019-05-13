735104909304b48f4b890e5dfd14ed8e429eb78c
jakob <jakobreichl@gmail.com>
2019-05-13 11:46:28 +0200

    Lösungen zur Linearen Algebra verbessert

--- prakt4/a1LinGln.m
+++ prakt4/a1LinGln.m
@@ -1,6 +1,9 @@
-clear
 % Aufgabe 1
 % Teilaufgabe a
+disp('a) Matrix A')
+
+
+disp('Vektor b')
 
 
 % Teilaufgabe b
@@ -8,7 +11,17 @@ disp('b) Determinante von A')
 
 
 % Teilaufgabe c
-disp('c) Lösung x berechnen')
+disp('c) Lösung x1 mit Linksdivision berechnen')
+
+
+disp('c) Lösung x2 mit mldivide berechnen')
+
+
+disp('c) Unterschied x1 und x2')
+disp('Länge: x1 - x2')
+
+
+disp('c) Probe: b1 = A*x1')
 
 
 % Teilaufgabe d
@@ -16,8 +29,21 @@ disp('d) Inverse Matrix von A')
 
 
 % Teilaufgabe e
-disp('e) Produkt AINV*A')
+disp('e) Produkt IA*A')
 
 
 % Teilaufgabe f
 disp('f) x mit inverser Matrix berechnen')
+
+
+% Teilaufgabe g
+
+
+disp('g) symmetrischer Anteil B:')
+
+
+disp('g) schiefsymmetrischer Anteil C:')
+
+
+disp('g) Summe B + C:')
+
--- prakt4/a2Eigenwert_lsg.m
+++ prakt4/a2Eigenwert_lsg.m
@@ -1,16 +1,13 @@
-clear
+clear; clc
 % Aufgabe 2
 A = [ 4 2 8 2;
       2 5 4 1;
       8 4 6 1;
       2 1 1 1];
-
 % Teilaufgabe a
-da = det(A);
-[V, D] = eig(A);
-
 disp('Determinante von A:')
-disp(da)  
+disp(det(A));
+[V, D] = eig(A);
 disp('Eigenwerte von A:')
 fprintf('%7.2f\n',diag(D))
 disp('Eigenvektoren von A:')
@@ -29,21 +26,21 @@ prd1 = prod(diag(D))
 disp('A*v2 - lambda2*v2')
 v2=V(:,2);
 fprintf('%15.10f\n',A*v2-D(2,2)*v2)
+fprintf('A*v2-l2*v2 : %15.10f\n',norm(A*v2-D(2,2)*v2))
 
 % Teilaufgabe d
 disp('Skalarprodukt von v1 und v2')
 v1=V(:,1);
-v2=V(:,2);
 fprintf('%15.10f\n',v1'*v2)
-
 disp('Länge von v1')
-v1= V(:,1);
 disp( norm( v1 ) )
 disp( sqrt( v1'*v1 ) )
 disp( sqrt( dot(v1,v1) ) )
 disp( sqrt( sum( v1.*v1 ) ) )
 
 % Teilaufgabe e
+disp('Vtransponiert*V')
+disp(V'*V)
 disp('Vtransponiert*A*V')
 disp(V'*A*V)
 
@@ -55,10 +52,9 @@ b = [1;2;3;4];
 a = V\b;
 % a = mldivide(V,b);
 disp('Koeffizienten der Eigenvektoren')
-fprintf('  a(1) =%10.6f\n',a(1))
-fprintf('  a(2) =%10.6f\n',a(2))
-fprintf('  a(3) =%10.6f\n',a(3))
-fprintf('  a(4) =%10.6f\n',a(4))
+for k=1:4
+  fprintf('  a(%d) =%10.6f\n',k,a(k))
+end
 
 disp('Verifikation der Zerlegung von b')
 %b1 = a(1)*V(:,1)+a(2)*V(:,2)+...+a(4)*V(:,4);
@@ -70,4 +66,4 @@ disp('Zerlegung von v1')
 v1 = V(:,1);
 av1 = V\v1;
 disp(av1)
-av2 =mldivide(V,v1)
+av2 = mldivide(V,v1)
